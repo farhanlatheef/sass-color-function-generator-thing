d0ccf825559482cdc9e680bbf1d885aece356e6e
Farhan ck <farhan.latheef1@gmail.com>
2023-06-23 20:54:44 +0530

    allow decimals

--- js/getColorFunction.js
+++ js/getColorFunction.js
@@ -28,24 +28,82 @@ function getColorFunction( colorString, hslDifferences, mode ){
     colorFunction = "darken( " + colorFunction + ", " + hslDifferences[2] + " )";
   }
 
-  console.log( hslDifferences );
+  // console.log( hslDifferences );
   return( colorFunction );
 
 }
 
+// const round = (num) => Math.round((num + Number.EPSILON) * 10) / 10
+const round = (num) => {
+  if(num == 0) return 0;
+  return Math.round(num * 10) / 10
+}
+
+const  HexToHSL = (hex) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  if (!result) {
+    throw new Error("Could not parse Hex Color tt"+ hex);
+  }
+
+  const rHex = parseInt(result[1], 16);
+  const gHex = parseInt(result[2], 16);
+  const bHex = parseInt(result[3], 16);
+
+  const r = rHex / 255;
+  const g = gHex / 255;
+  const b = bHex / 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+
+  let h = (max + min) / 2;
+  let s = h;
+  let l = h;
+
+  if (max === min) {
+    // Achromatic
+    return [0,0,l*100];
+  }
+
+  const d = max - min;
+  s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+  switch (max) {
+    case r:
+      h = (g - b) / d + (g < b ? 6 : 0);
+      break;
+    case g:
+      h = (b - r) / d + 2;
+      break;
+    case b:
+      h = (r - g) / d + 4;
+      break;
+  }
+  h /= 6;
+
+  s = s * 100;
+  l = l * 100;
+  h = 360 * h;
+
+  return [ h, s, l ];
+}
+
+
+
 function getColorDifferences( start, end ) {
 
   var differences = [];
+  var startColorHSL = HexToHSL(start);
+  var endColorHSL = HexToHSL(end);
 
-  var startColor = Color( start );
-  var endColor = Color( end );
-
-  var startColorHSL = startColor.hslArray();
-  var endColorHSL = endColor.hslArray();
+  console.log('start', startColorHSL);
+  console.log('end', endColorHSL);
 
   for ( var i = 0; i < 3; i++ ) {
-    differences[i] = startColorHSL[i] - endColorHSL[i];
+    differences[i] = round(startColorHSL[i] - endColorHSL[i]);
   }
+
+  console.log(differences);
 
   return( differences );
 
@@ -61,5 +119,7 @@ $(function(){
     var differences = getColorDifferences( startColor, endColor );
     var outputText = getColorFunction( startColor, differences );
     $( "#functionOutput" ).text(outputText);
+    console.log('tt', differences.map(i=>-i));
+    $( "#functionOutput2" ).text(differences.map(i=>-i).join(', '));
   });
 });
